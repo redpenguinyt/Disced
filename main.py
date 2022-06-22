cf455213cfafca844ecb7a243ba762085c4243af
Sourcery AI <>
2022-06-22 13:58:53 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -7,29 +7,28 @@ from discord_webhook import DiscordWebhook
 
 @guildedbot.event
 async def on_message(message):
-	if not "Gil" in message.author.name:
-		if message.channel.id == "97b0c27c-aa0a-4c2a-9c42-414982d19506":
-			print(f"Guilded > {message.author.name} > {message.content}")
-			await send_to_discord(
-				os.environ['discord-webhook'],
-				message.author.name,
-				message.author.avatar.url,
-				message.content
-			)
+	if ("Gil" not in message.author.name
+	    and message.channel.id == "97b0c27c-aa0a-4c2a-9c42-414982d19506"):
+		print(f"Guilded > {message.author.name} > {message.content}")
+		await send_to_discord(
+			os.environ['discord-webhook'],
+			message.author.name,
+			message.author.avatar.url,
+			message.content
+		)
 
 # Discord
 
 @discordbot.event
 async def on_message(message):
-	if not message.author.bot:
-		if message.channel.id == 884525097677910033:
-			print(f"Discord > {message.author.name} > {message.content}")
-			await send_to_guilded(
-				os.environ["guilded-webhook"],
-				message.author.name,
-				message.author.avatar_url,
-				message.content
-			)
+	if not message.author.bot and message.channel.id == 884525097677910033:
+		print(f"Discord > {message.author.name} > {message.content}")
+		await send_to_guilded(
+			os.environ["guilded-webhook"],
+			message.author.name,
+			message.author.avatar_url,
+			message.content
+		)
 
 # Run
 
